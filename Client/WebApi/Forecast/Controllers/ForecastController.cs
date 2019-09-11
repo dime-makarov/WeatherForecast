0ad033a9c8dcaa03be4a26341fd61c404e4cc300
Dmitry Makarov <Dmitry.Makarov.GDC@ts.fujitsu.com>
2019-09-11 15:52:31 +0300

    ForecastController.

--- Client/WebApi/Forecast/Controllers/ForecastController.cs
+++ Client/WebApi/Forecast/Controllers/ForecastController.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Web;
+using System.Web.Configuration;
 using System.Web.Http;
+using Dm.WeatherForecast.Client.ForecastService;
+using WcfContract = Dm.WeatherForecast.Service.Wcf.Contract;
 
 namespace Dm.WeatherForecast.Client.WebApi.Forecast.Controllers
 {
     public class ForecastController : ApiController
     {
         [HttpGet]
-        public string Get()
+        public List<WcfContract.Forecast> Get([FromUri]int cityId, [FromUri]DateTime targetDate)
         {
-            return "Forecast";
+            List<WcfContract.Forecast> forecasts;
+            string hostName = WebConfigurationManager.AppSettings["ForecastServiceHostName"];
+
+            using (ForecastServiceClient client = new ForecastServiceClient(hostName))
+            {
+                forecasts = client.GetForecast(cityId, targetDate);
+            }
+
+            return forecasts;
         }
     }
 }
